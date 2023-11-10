0d56133c67a7a53f06014a3e6d7f5e76b770cfbb
Akrishna521 <anishetty.saikrishna@mphasis.com>
2023-11-10 15:52:16 +0530

    Assignment6-3

---
+++ ConAppAssignmentArrayList/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+class Program
+{
+    static void Main()
+    {
+        ArrayList studentGrades;
+        studentGrades = new ArrayList() { 85, 92, 78, 95, 88 };
+        Console.WriteLine("Student Grades:");
+        foreach (var grade in studentGrades)
+        {
+            Console.Write(grade + " ");
+        }
+        Console.ReadLine();
+    }
+}
